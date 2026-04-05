6e7cbafd81ffcd4d0c4df2a0f04962a2752195c8
Ihor Lisnyi <lisnyihor@gmail.com>
2026-04-05 22:46:32 +0300

    Fix RSS feed for i18n

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,16 +1,18 @@
 import { getCollection } from 'astro:content';
 import rss from '@astrojs/rss';
-import { SITE_DESCRIPTION, SITE_TITLE } from '../consts';
+import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
+import { filterByLocale } from '../i18n/content';
 
 export async function GET(context) {
-	const posts = await getCollection('blog');
-	return rss({
-		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
-		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/blog/${post.id}/`,
-		})),
-	});
+  const allPosts = await getCollection('blog');
+  const posts = filterByLocale(allPosts, 'uk');
+  return rss({
+    title: SITE_TITLE['uk'],
+    description: SITE_DESCRIPTION['uk'],
+    site: context.site,
+    items: posts.map((post) => ({
+      ...post.data,
+      link: `/blog/${post.id.replace('uk/', '')}/`,
+    })),
+  });
 }
