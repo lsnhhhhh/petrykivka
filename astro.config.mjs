09e37ec3cd385218590ad8413f92db1d022c1d20
Ihor Lisnyi <lisnyihor@gmail.com>
2026-04-20 14:46:15 +0300

    Update site URL to petrykivka.org

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://petrykivka.org',
 	integrations: [mdx(), sitemap()],
 	i18n: {
 		defaultLocale: 'uk',
